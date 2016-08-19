c777bce9aa11805489c853aa1fe07a12a5391868
Robert Chen <rbchen16@gmail.com>
2016-08-19 01:07:24 -0400

    Able to print entire list of champions as table row in view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var dbGeneratorFactory = require("../scripts.js");
 var dbGenerator = new dbGeneratorFactory("5496afde-249a-4ca5-9ac8-38d7e7316a5c");
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
@@ -19,10 +18,13 @@ router.post('/dbdisplay', function(req, res, next){
 
   var db = req.db;
   var collection = db.get('synergycollection');
-  collection.find({},{},function(e,docs){
+  dbGenerator.getChampionKeyList(dbGenerator, function(championKeyList){
+    collection.find({},{},function(e,docs){
       res.render('dbdisplay', {
+          "championKeyList" : championKeyList,
           "synergyEntries" : docs
       });
+    });
   });
 });
 
--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ dbGenerator.prototype.populateDb = function(req, res) {
 
   var db = req.db;
   var collection = db.get('synergycollection');
-
+  /*
   // Submit to the DB
   var toInsert = [{ "champion" : "Lucian",
                     "winrate" : "50%",
@@ -21,16 +21,15 @@ dbGenerator.prototype.populateDb = function(req, res) {
                       "Jinx" : "50%"
                     }
                   }];
-  collection.insert(toInsert);
+  collection.insert(toInsert);*/
 }
 
 dbGenerator.prototype.getAllChampions = function(callback) {
   var request = require('request');
   request("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=" + this.apiKey, function(error, response, body){
       championList = JSON.parse(body).data;
-      var id = callback(championList);
-      console.log("Output id: " + id);
-      return id;
+      console.log("expected 3 " + championList['Sona']['title']);
+      callback(championList);
   });
 }
 
@@ -55,6 +54,17 @@ dbGenerator.prototype.getChampionIdFromKey = function(dbGenerator, keyToFind) {
       }
     }
     return -1;
+  });
+}
+
+dbGenerator.prototype.getChampionKeyList = function(dbGenerator, callback){
+  dbGenerator.getAllChampions(function(championList){
+    var numChampions = Object.keys(championList).length;
+    var championKeyList = [];
+    for (var championEntry in championList){
+      championKeyList.push(championList[championEntry]["key"]);
+    }
+    callback(championKeyList);
   });
 }
 
