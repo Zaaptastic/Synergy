4d88bfc212396020642ee30db5944479a578d6a8
Robert Chen <rbchen16@gmail.com>
2016-09-01 13:22:47 -0400

    Can also remove now

--- public/javascripts/event-scripts.js
+++ public/javascripts/event-scripts.js
@@ -5,12 +5,22 @@ $(document).ready(function(){
     	//the button belonged to ally-container or enemy-container
     	var ally = $(this).parent().hasClass('ally-container');
     	var text = $(this).text();
-    	//$('.recommended-container').append('div').addClass('champion-button').attr('id',text).text(text);
+    	
+    	//Just for development, make corresponding elem appear
     	$('<div></div>').attr('id',text).addClass('champion-button')
     		.text(text).appendTo($('.recommended-container'));
 
+    	//Finally, add the 'fade' class to mark the above as completed
     	$(this).addClass('fade');
     }else{
+    	//Button is currently selected, toggle off. First identify whether
+    	//the button belonged to ally-container or enemy-container
+    	var ally = $(this).parent().hasClass('ally-container');
+    	var text = $(this).text();
+
+    	//Just for development, make corresponding elem disappear
+    	$('#'+text).remove();
+
     	$(this).removeClass('fade');
     }
   });
